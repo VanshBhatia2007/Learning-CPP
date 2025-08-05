869a7b83f0ec76bb4cdd30c52e5dd14ca7d24495
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-08-05 16:51:34 +0530

    added string

--- Topics/identifiers/taking_input.cpp
+++ Topics/identifiers/taking_input.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
     //to take input from the user
     //we'll create a bucket/variable to store the input inside it
-    string name;
-    cin >> name; //cin is used to take input from the user
-    cout << name; //cout is used to print the output to the console
-    int a;
+    int a =0;
     cin >> a; //cin is used to take input from the user
     cout << a; //cout is used to print the output to the console
     return 0;
