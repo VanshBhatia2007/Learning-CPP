5ac084c1d4217bb348873d2f0275f5fc856dd784
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-08-10 15:34:08 +0530

    bool added

--- Topics/identifiers/taking_input.cpp
+++ Topics/identifiers/taking_input.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     //to take input from the user
     //we'll create a bucket/variable to store the input inside it
-    int a =0;
+    int a = 0;
     cin >> a; //cin is used to take input from the user
-    cout << a; //cout is used to print the output to the console
+    cout << "you entered: "<<a; //cout is used to print the output to the console
     return 0;
 }
--- Topics/introduction/syntax.cpp
+++ Topics/introduction/syntax.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-
+#include <iostream> //header file for input/output functionality
+using namespace std; 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 }
