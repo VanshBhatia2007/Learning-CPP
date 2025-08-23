cb792dcda60a2ba2361dd73d96686eeee1f01ac7
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-08-23 23:54:11 +0530

    added new topics

---
+++ Topics/scoop/scoop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int b=100;
+
+int main(){
+    int i;
+    
+    cout<<i<<endl;
+    cout<<::b<<endl;
+    
+    return 0;
+}
---
+++ Topics/ternary/syntax.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int a=-1;
+     //syntax : (condition) ? work:work
+     a>0 ? cout<<"hello" : cout<<"no";
+
+    return 0;
+}
